5c1a98fdf089fb6d4c6288d7e7d91c0d77b8dd94
Andres Juan <andres.giraldov@upb.edu.co>
2024-02-29 23:21:41 -0500

    Ejercicio18 empezado

--- Ejercicio18/Ejercicio18/Ejercicio18.ino
+++ Ejercicio18/Ejercicio18/Ejercicio18.ino
@@ -1,9 +1,45 @@
-void setup() {
-  // put your setup code here, to run once:
+void task1()
+{
+    enum class Task1States    {
+        INIT,
+        WAIT_DATA
+    };
 
+    static Task1States task1State = Task1States::INIT;
+    static uint32_t value;
+
+    switch (task1State)
+    {
+    case Task1States::INIT:
+    {
+        Serial.begin(115200);
+        task1State = Task1States::WAIT_DATA;
+        break;
+    }
+
+    case Task1States::WAIT_DATA:
+    {
+       if (Serial.available() > 0)
+        {
+            value = value + Serial.available();
+            //static uint32_t bufferRecepcion[value];
+            Serial.print("Se han recibido");
+            Serial.print(value);
+            Serial.print('\n');
+        }
+        break;
+    }
+
+    default:
+    {
+        break;
+    }
+    }
+}
+
+void setup() {
+  task1();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
 }
